2b7b29c024928d3cb0c289b4fdacf30e571379cc
Manuelf <manuelf0710@gmail.com>
2021-12-09 20:30:05 -0500

    elegir idioma en tutorial

--- src/app/components/utils/components/tutorial/tutorial.component.ts
+++ src/app/components/utils/components/tutorial/tutorial.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core'
-import { UiService } from 'src/app/services/ui.service'
+import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-tutorial',
@@ -7,26 +8,33 @@ import { UiService } from 'src/app/services/ui.service'
   styleUrls: ['./tutorial.component.scss'],
 })
 export class TutorialComponent implements OnInit {
-  public step: number = 1
-  public admin: boolean = true
-  public lang: string
+  public step: number = 1;
+  public admin: boolean = true;
+  public lang: string;
+  private languajeSetTutorial = new Subject<string>();
   constructor(private ui: UiService) {}
 
   ngOnInit(): void {
-    this.lang = localStorage.getItem('lang') || 'Esp'
+    this.lang = localStorage.getItem('lang') || 'Esp';
   }
 
   closeTutorial() {
-    this.ui.dismissModal(TutorialComponent)
+    this.ui.dismissModal(TutorialComponent);
   }
   next() {
-    ++this.step
+    ++this.step;
     if (!this.admin && this.step == 5) {
-      this.step = 6
+      this.step = 6;
     }
   }
   save() {
-    localStorage.setItem('tutorial', 'done')
-    this.closeTutorial()
+    localStorage.setItem('tutorial', 'done');
+    this.closeTutorial();
+  }
+  languageTutorial(languaje: string) {
+    this.lang = languaje;
+    localStorage.setItem('lang', languaje);
+    this.languajeSetTutorial.next(languaje);
+    this.next();
   }
 }
